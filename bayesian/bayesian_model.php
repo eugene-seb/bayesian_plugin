0c28e49167517fea3cb4118ac03a7fd6b0118942
Eugène Sébastien <etoundisebastien@gmail.com>
2023-06-02 12:44:38 +0200

    Fin duprojet

--- bayesian/bayesian_model.php
+++ bayesian/bayesian_model.php
@@ -1,24 +1,21 @@
 <?php
 
 /**
- * Permet de calculer la difficulté de chaque question à partir du taux de réussite.
- * 
- * La difficulté sera comprise entre 0 et 1, où 0 représente une question 
- * facile (toutes les réponses sont correctes) et 1 représente une question 
- * difficile (aucune réponse correcte).
+ * Permet de calculer la probabilité à priori de chaque question à partir 
+ * du taux de réussite d'une évaluation précédente du même QCM.
  * 
  * @* @param array const les réponses correctes
  * @* @param array const les réponses de l'étudiant
  */
-function prob_difficultes($reponses_corrects, $reponses_candidats) {
+function les_prop_apriori($reponses_corrects, $reponses_candidats) {
 	
 	$nbr_questions = count($reponses_corrects);
 	$nbr_candidat = count($reponses_candidats);
 
 	// $nbr_reponse_choisit = nbr de fois où une réponse correct a été choisit par question
-	// $difficultes = difficulté de chaque question
+	// $props_apriori = prop_apriori de chaque question
 	// initialisation à 0
-	$difficultes = $nbr_reponse_choisit = array_fill(0, $nbr_questions, 0);
+	$props_apriori = $nbr_reponse_choisit = array_fill(0, $nbr_questions, 0);
 
 	for ($i = 0; $i < $nbr_candidat; $i++) {	// Parcours chaque candidat
 
@@ -32,14 +29,14 @@ function prob_difficultes($reponses_corrects, $reponses_candidats) {
 		}
 	}
 
-	// On calcul la difficulté de chaque question
+	// On calcul la probabilité à priori de chaque question en utilisant 
+	// la formule du taux de réussite
 	for ($i = 0; $i < $nbr_questions; $i++) {
 
-		// difficulte = 1 -  taux de réussite
-		$difficultes[$i] = 1 - ($nbr_reponse_choisit[$i] / $nbr_candidat);
+		$props_apriori[$i] = $nbr_reponse_choisit[$i] / $nbr_candidat;
 	}
 
-	return $difficultes;
+	return $props_apriori;
 }
 
 
@@ -48,96 +45,368 @@ function prob_difficultes($reponses_corrects, $reponses_candidats) {
  * en fonction de ses connaissances à priori, ses réponses et la difficulté des questions.
  * Sur cette évaluation, les questions sont indépendantes.
  * 
+ * L'utilisation de la probabilité que l'élève ait les connaissances nécessaires pour
+ * répondre correctement à chaque question permet de prendre en compte la difficulté 
+ * de chaque question et le niveau de l'élève.
+ * 
+ * La difficulté de chaque question est estimée par le professeur, qui pourra par 
+ * exemple avoir le barême suivant: niveau facile(1) intermédiare(0.5) très_difficile(0) 
+ * 
  * @* @param array const les réponses correctes
  * @* @param array var les difficultés de chaque question
  * @* @param array var les réponses de l'étudiant
  */
-function bayesian_eval($reponses_correctes, $difficultes, $reponses_eleve){
-	
-	
-	// Elle doit normalement être mise à jour pour chaque question (quiz à questions indépendantes)
-	// par une IA qui étudiera les réponses des candidats à cette question lors des anciennes questions.
+function bayesian_eval($reponses_correctes, $difficultes, $reponses_eleve, $reponses_prec_eleves){
 
-
-
-	/* la probabilité a priori que l'élève réponde correctement à une question sans
+	/* 
+		la probabilité a priori que l'élève réponde correctement à une question sans
 		tenir compte de ses réponses précédentes. C'est une valeur que l'on fixe de façon arbitraire.  
 		Par exemple, si l'on estime que les élèves ont de bonnes connaissances et ont plus de chances
 		de répondre correctement (par exemple, 70% de chances de réussite a priori), 
 		alors on peut définir "$prior_prob" à 0.7. 
 
-		Pour ce test, on fixera cette valeur à 0.5 car nous partons sur la base de la probabilité du
-		choix d'une réponse. Ici nos questions aurons 2 réponses, donc probabilité == 1/2.
+		Elle doit normalement être mise à jour pour chaque question (quiz à questions indépendantes)
+		par une IA qui étudiera les réponses des candidats à cette question lors des anciennes questions.
+	
+		Pour ce test, on supposera qu'un élève ai 50% de chance de répondre correctement à chaque question.
 	*/
-	$prior_prob = 0.5;
+	$prior_prob = les_prop_apriori($reponses_correctes, $reponses_prec_eleves);
 
 	$note_totale = $note_partielle = 0;
 
-	// On boucle sur chaque question
-	for ($i = 0; $i < count($reponses_correctes); $i++) {
-		// On récupère la difficulté de la question
-		$difficulte = $difficultes[$i];
+	$note_finale = array_fill(0, count($reponses_eleve), 0);
 
-		// Dépendamment de la difficulté de la question
-		if($reponses_eleve[$i] == $reponses_correctes[$i] && (1 - $difficulte) <= 0) { // Si la difficulté était élevé on fait un bonus
-			$p_ba = 1 + $difficulte; 
-		} 
-		else if($reponses_eleve[$i] == $reponses_correctes[$i]) {
-			$p_ba = 1; 
-		} 
-		// si il a raté une question difficile on lui donne quand même une partie de la note
-		else if($reponses_eleve[$i] != $reponses_correctes[$i] && (1 - $difficulte) <= 0) {
-			$p_ba = (1 - $difficulte);
-		} 
-		else { // Sinon il a raté une question facile
-			$p_ba = 0;
+	for ($x = 0; $x < count($reponses_eleve); $x++) {
+
+		// On boucle sur chaque question
+		for ($i = 0; $i < count($reponses_correctes); $i++) {
+
+			// On récupère la difficulté de la question
+			$difficulte = $difficultes[$i];
+
+			// Dépendamment de la difficulté de la question on détermine 
+			// que l'élève ait la réponse correcte
+			$p_ba = ($reponses_eleve[$x][$i] == $reponses_correctes[$i]) ? (1 - $difficulte) : $difficulte;
+
+			$p_b = $p_ba * $prior_prob[$i] + (1 - $p_ba) * (1 - $prior_prob[$i]);
+			$p_ab = ($p_ba * $prior_prob[$i]) / $p_b; 
+
+			// On calcule la note partielle de l'élève pour cette question en utilisant la difficulté de la question
+			// On multiplie la probabilité de réussite par le nombre de poids que vaut la question
+			$note_partielle = $p_ab * 1; 
+
+			// On ajoute la note partielle à la note totale de l'élève
+			$note_totale += $note_partielle;
+
+			$note_partielle = 0;
 		}
 
-		$p_ba = $reponses_eleve[$i] == $reponses_correctes[$i] ? 5: 5;
-
-		$p_b = $p_ba * $prior_prob + (1 - $p_ba) * (1 - $prior_prob);
-		$p_a_b = ($p_ba * $prior_prob) / $p_b;
-
-		// On calcule la note partielle de l'élève pour cette question en utilisant la difficulté de la question
-		$note_partielle = $p_a_b; // On multiplie la note partielle par la difficulté de la question
-
-		// On ajoute la note partielle à la note totale de l'élève
-		$note_totale += $note_partielle;
-
-		$note_partielle = 0;
+		$note_finale[$x] = $note_totale / count($reponses_correctes) * 20; // On suppose que la note finale est sur 20
+		$note_totale=0;
 	}
-
-	$note_finale = $note_totale / count($reponses_correctes) * 20; // On suppose que la note finale est sur 20
 
 	return $note_finale;
 
 }
+
 //-------------------------------------------------------------------------------------------------------------
 
+// Les réponse des élèves lors de la session précédente
+$reponses_prec_eleves = [[2,1,3,4,2,1,3,4,2,1],
+						[2,1,3,4,2,1,3,4,5,1],
+						[2,1,3,4,2,1,3,4,2,1],
+						[2,1,0,4,2,1,3,4,2,1],
+						[2,1,3,4,2,1,3,4,2,1],
+						[2,1,3,4,2,1,3,4,2,1],
+						[2,1,3,4,2,7,3,4,2,1],
+						[2,1,3,4,2,1,3,4,2,1],
+						[2,1,3,4,2,1,3,4,2,1],
+						[2,1,3,4,2,1,3,4,2,1],
+						[2,1,3,4,2,1,3,4,2,1],
+						[2,1,3,4,2,1,3,4,5,1],
+						[2,1,3,4,2,1,3,4,2,1],
+						[2,1,0,4,2,1,3,4,2,1],
+						[2,1,3,4,2,1,3,4,2,1],
+						[0,1,2,3,4,5,6,7,8,9],
+						[0,1,2,3,4,5,6,7,8,9],
+						[0,1,2,3,4,5,6,7,8,9],
+						[0,1,2,3,4,5,6,7,8,9],
+						[0,1,2,3,4,5,6,7,8,9],
+						[0,1,2,3,4,5,6,7,8,9],
+						[0,1,2,3,4,5,6,7,8,9],
+						[0,1,2,3,4,5,6,7,8,9],
+						[2,1,3,4,2,7,3,4,2,1],
+						[2,1,3,4,2,1,3,4,2,1],
+						[2,1,0,4,2,1,3,4,2,1],
+						[2,1,3,4,2,1,3,4,2,1],
+						[2,1,3,4,2,1,3,4,2,1],
+						[2,1,3,4,2,7,3,4,2,1],
+						[2,1,3,4,2,1,3,4,2,1],
+						[2,1,3,4,2,1,3,4,2,1],
+						[2,1,3,4,2,1,3,4,2,1],
+						[2,1,3,4,2,1,3,4,2,1],
+						[2,1,3,4,2,1,3,4,5,1],
+						[2,1,3,4,2,1,3,4,2,1],
+						[2,1,0,4,2,1,3,4,2,1],
+						[2,1,3,4,2,1,3,4,2,1],
+						[0,1,2,3,4,5,6,7,8,9],
+						[0,1,2,3,4,5,6,7,8,9],
+						[0,1,2,3,4,5,6,7,8,9],
+						[0,1,2,3,4,5,6,7,8,9],
+						[0,1,2,3,4,5,6,7,8,9],
+						[0,1,2,3,4,5,6,7,8,9],
+						[0,1,2,3,4,5,6,7,8,9],
+						[0,1,2,3,4,5,6,7,8,9],
+						[2,1,3,4,2,7,3,4,2,1],
+						[2,1,3,4,2,1,3,4,2,1],
+						[2,1,0,4,2,1,3,4,2,1],
+						[2,1,3,4,2,1,3,4,2,1],
+						[2,1,3,4,2,1,3,4,2,1],
+						[2,1,3,4,2,7,3,4,2,1],
+						[2,1,3,4,2,1,3,4,2,1],
+						[2,1,3,4,2,1,3,4,2,1],
+						[2,1,3,4,2,1,3,4,2,1],
+						[2,1,3,4,2,1,3,4,2,1],
+						[2,1,3,4,2,1,3,4,5,1],
+						[2,1,3,4,2,1,3,4,2,1],
+						[2,1,0,4,2,1,3,4,2,1],
+						[2,1,3,4,2,1,3,4,2,1],
+						[0,1,2,3,4,5,6,7,8,9],
+						[0,1,2,3,4,5,6,7,8,9],
+						[0,1,2,3,4,5,6,7,8,9],
+						[0,1,2,3,4,5,6,7,8,9],
+						[0,1,2,3,4,5,6,7,8,9],
+						[0,1,2,3,4,5,6,7,8,9],
+						[0,1,2,3,4,5,6,7,8,9],
+						[0,1,2,3,4,5,6,7,8,9],
+						[2,1,3,4,2,7,3,4,2,1],
+						[2,1,3,4,2,1,3,4,2,1],
+						[2,1,0,4,2,1,3,4,2,1],
+						[2,1,3,4,2,1,3,4,2,1],
+						[2,1,3,4,2,1,3,4,2,1],
+						[2,1,3,4,2,7,3,4,2,1],
+						[2,1,3,4,2,1,3,4,2,1],
+						[2,1,3,4,2,1,3,4,2,1],
+						[2,1,3,4,2,1,3,4,2,1],
+						[2,1,3,4,2,1,3,4,2,1],
+						[2,1,3,4,2,1,3,4,5,1],
+						[2,1,3,4,2,1,3,4,2,1],
+						[2,1,0,4,2,1,3,4,2,1],
+						[2,1,3,4,2,1,3,4,2,1],
+						[0,1,2,3,4,5,6,7,8,9],
+						[0,1,2,3,4,5,6,7,8,9],
+						[0,1,2,3,4,5,6,7,8,9],
+						[0,1,2,3,4,5,6,7,8,9],
+						[0,1,2,3,4,5,6,7,8,9],
+						[0,1,2,3,4,5,6,7,8,9],
+						[0,1,2,3,4,5,6,7,8,9],
+						[0,1,2,3,4,5,6,7,8,9],
+						[2,1,3,4,2,7,3,4,2,1],
+						[2,1,3,4,2,1,3,4,2,1],
+						[2,1,0,4,2,1,3,4,2,1],
+						[2,1,3,4,2,1,3,4,2,1],
+						[2,1,3,4,2,1,3,4,2,1],
+						[2,1,3,4,2,7,3,4,2,1],
+						[2,1,3,4,2,1,3,4,2,1],
+						[2,1,3,4,2,1,3,4,2,1],
+						[2,1,3,4,2,1,3,4,2,1],
+						[2,1,3,4,2,1,3,4,2,1],
+						[2,1,3,4,2,1,3,4,5,1],
+						[2,1,3,4,2,1,3,4,2,1],
+						[2,1,0,4,2,1,3,4,2,1],
+						[2,1,3,4,2,1,3,4,2,1],
+						[0,1,2,3,4,5,6,7,8,9],
+						[0,1,2,3,4,5,6,7,8,9],
+						[0,1,2,3,4,5,6,7,8,9],
+						[0,1,2,3,4,5,6,7,8,9],
+						[0,1,2,3,4,5,6,7,8,9],
+						[0,1,2,3,4,5,6,7,8,9],
+						[0,1,2,3,4,5,6,7,8,9],
+						[0,1,2,3,4,5,6,7,8,9],
+						[2,1,3,4,2,7,3,4,2,1],
+						[2,1,3,4,2,1,3,4,2,1],
+						[2,1,0,4,2,1,3,4,2,1],
+						[2,1,3,4,2,1,3,4,2,1],
+						[2,1,3,4,2,1,3,4,2,1],
+						[2,1,3,4,2,7,3,4,2,1],
+						[2,1,3,4,2,1,3,4,2,1],
+						[2,1,3,4,2,1,3,4,2,1],
+						[2,1,3,4,2,1,3,4,2,1],
+						[2,1,3,4,2,1,3,4,2,1],
+						[2,1,3,4,2,1,3,4,5,1],
+						[2,1,3,4,2,1,3,4,2,1],
+						[2,1,0,4,2,1,3,4,2,1],
+						[2,1,3,4,2,1,3,4,2,1],
+						[0,1,2,3,4,5,6,7,8,9],
+						[0,1,2,3,4,5,6,7,8,9],
+						[0,1,2,3,4,5,6,7,8,9],
+						[0,1,2,3,4,5,6,7,8,9],
+						[0,1,2,3,4,5,6,7,8,9],
+						[0,1,2,3,4,5,6,7,8,9],
+						[0,1,2,3,4,5,6,7,8,9],
+						[0,1,2,3,4,5,6,7,8,9],
+						[2,1,3,4,2,7,3,4,2,1],
+						[2,1,3,4,2,1,3,4,2,1],
+						[2,1,0,4,2,1,3,4,2,1],
+						[2,1,3,4,2,1,3,4,2,1],
+						[2,1,3,4,2,1,3,4,2,1],
+						[2,1,3,4,2,7,3,4,2,1],
+						[2,1,3,4,2,1,3,4,2,1],
+						[2,1,3,4,2,1,3,4,2,1],
+						[2,1,3,4,2,1,3,4,2,1],
+						[2,1,3,4,2,1,3,4,2,1],
+						[2,1,3,4,2,1,3,4,5,1],
+						[2,1,3,4,2,1,3,4,2,1],
+						[2,1,0,4,2,1,3,4,2,1],
+						[2,1,3,4,2,1,3,4,2,1],
+						[0,1,2,3,4,5,6,7,8,9],
+						[0,1,2,3,4,5,6,7,8,9],
+						[0,1,2,3,4,5,6,7,8,9],
+						[0,1,2,3,4,5,6,7,8,9],
+						[0,1,2,3,4,5,6,7,8,9],
+						[0,1,2,3,4,5,6,7,8,9],
+						[0,1,2,3,4,5,6,7,8,9],
+						[0,1,2,3,4,5,6,7,8,9],
+						[2,1,3,4,2,7,3,4,2,1],
+						[2,1,3,4,2,1,3,4,2,1],
+						[2,1,0,4,2,1,3,4,2,1],
+						[2,1,3,4,2,1,3,4,2,1],
+						[2,1,3,4,2,1,3,4,2,1],
+						[2,1,3,4,2,7,3,4,2,1],
+						[2,1,3,4,2,1,3,4,2,1],
+						[2,1,3,4,2,1,3,4,2,1],
+						[2,1,3,4,2,1,3,4,2,1],
+						[2,1,3,4,2,1,3,4,2,1],
+						[2,1,3,4,2,1,3,4,5,1],
+						[2,1,3,4,2,1,3,4,2,1],
+						[2,1,0,4,2,1,3,4,2,1],
+						[2,1,3,4,2,1,3,4,2,1],
+						[0,1,2,3,4,5,6,7,8,9],
+						[0,1,2,3,4,5,6,7,8,9],
+						[0,1,2,3,4,5,6,7,8,9],
+						[0,1,2,3,4,5,6,7,8,9],
+						[0,1,2,3,4,5,6,7,8,9],
+						[0,1,2,3,4,5,6,7,8,9],
+						[0,1,2,3,4,5,6,7,8,9],
+						[0,1,2,3,4,5,6,7,8,9],
+						[2,1,3,4,2,7,3,4,2,1],
+						[2,1,3,4,2,1,3,4,2,1],
+						[2,1,0,4,2,1,3,4,2,1],
+						[2,1,3,4,2,1,3,4,2,1],
+						[2,1,3,4,2,1,3,4,2,1],
+						[2,1,3,4,2,7,3,4,2,1],
+						[2,1,3,4,2,1,3,4,2,1],
+						[2,1,3,4,2,1,3,4,2,1],
+						[2,1,3,4,2,1,3,4,2,1],
+						[2,1,3,4,2,1,3,4,2,1],
+						[2,1,3,4,2,1,3,4,5,1],
+						[2,1,3,4,2,1,3,4,2,1],
+						[2,1,0,4,2,1,3,4,2,1],
+						[2,1,3,4,2,1,3,4,2,1],
+						[0,1,2,3,4,5,6,7,8,9],
+						[0,1,2,3,4,5,6,7,8,9],
+						[0,1,2,3,4,5,6,7,8,9],
+						[0,1,2,3,4,5,6,7,8,9],
+						[0,1,2,3,4,5,6,7,8,9],
+						[0,1,2,3,4,5,6,7,8,9],
+						[0,1,2,3,4,5,6,7,8,9],
+						[0,1,2,3,4,5,6,7,8,9],
+						[2,1,3,4,2,7,3,4,2,1],
+						[2,1,3,4,2,1,3,4,2,1],
+						[2,1,0,4,2,1,3,4,2,1],
+						[2,1,3,4,2,1,3,4,2,1],
+						[2,1,3,4,2,1,3,4,2,1],
+						[2,1,3,4,2,7,3,4,2,1],
+						[2,1,3,4,2,1,3,4,2,1],
+						[2,1,3,4,2,1,3,4,2,1],
+						[2,1,3,4,2,1,3,4,2,1],
+						[2,1,3,4,2,1,3,4,2,1],
+						[2,1,3,4,2,1,3,4,5,1],
+						[2,1,3,4,2,1,3,4,2,1],
+						[2,1,0,4,2,1,3,4,2,1],
+						[2,1,3,4,2,1,3,4,2,1],
+						[0,1,2,3,4,5,6,7,8,9],
+						[0,1,2,3,4,5,6,7,8,9],
+						[0,1,2,3,4,5,6,7,8,9],
+						[0,1,2,3,4,5,6,7,8,9],
+						[0,1,2,3,4,5,6,7,8,9],
+						[0,1,2,3,4,5,6,7,8,9],
+						[0,1,2,3,4,5,6,7,8,9],
+						[0,1,2,3,4,5,6,7,8,9],
+						[2,1,3,4,2,7,3,4,2,1],
+						[2,1,3,4,2,1,3,4,2,1],
+						[2,1,0,4,2,1,3,4,2,1],
+						[2,1,3,4,2,1,3,4,2,1],
+						[2,1,3,4,2,1,3,4,2,1],
+						[2,1,3,4,2,7,3,4,2,1],
+						[2,1,3,4,2,1,3,4,2,1],
+						[2,1,3,4,2,1,3,4,2,1],
+						[2,1,3,4,2,1,3,4,2,1],
+						[2,1,3,4,2,1,3,4,2,1],
+						[2,1,3,4,2,1,3,4,5,1],
+						[2,1,3,4,2,1,3,4,2,1],
+						[2,1,0,4,2,1,3,4,2,1],
+						[2,1,3,4,2,1,3,4,2,1],
+						[0,1,2,3,4,5,6,7,8,9],
+						[0,1,2,3,4,5,6,7,8,9],
+						[0,1,2,3,4,5,6,7,8,9],
+						[0,1,2,3,4,5,6,7,8,9],
+						[0,1,2,3,4,5,6,7,8,9],
+						[0,1,2,3,4,5,6,7,8,9],
+						[0,1,2,3,4,5,6,7,8,9],
+						[0,1,2,3,4,5,6,7,8,9],
+						[2,1,3,4,2,7,3,4,2,1],
+						[2,1,3,4,2,1,3,4,2,1],
+						[2,1,0,4,2,1,3,4,2,1],
+						[2,1,3,4,2,1,3,4,2,1],
+						[2,1,3,4,2,1,3,4,2,1],
+						[2,1,3,4,2,7,3,4,2,1],
+						[2,1,3,4,2,1,3,4,2,1],
+						[2,1,3,4,2,1,3,4,2,1],
+						[2,1,3,4,2,1,3,4,2,1],
+						[2,1,3,4,2,1,3,4,2,1],
+						[2,1,3,4,2,1,3,4,5,1],
+						[2,1,3,4,2,1,3,4,2,1],
+						[2,1,0,4,2,1,3,4,2,1],
+						[2,1,3,4,2,1,3,4,2,1],
+						[0,1,2,3,4,5,6,7,8,9],
+						[0,1,2,3,4,5,6,7,8,9],
+						[0,1,2,3,4,5,6,7,8,9],
+						[0,1,2,3,4,5,6,7,8,9],
+						[0,1,2,3,4,5,6,7,8,9],
+						[0,1,2,3,4,5,6,7,8,9],
+						[0,1,2,3,4,5,6,7,8,9],
+						[0,1,2,3,4,5,6,7,8,9],
+						[2,1,3,4,2,7,3,4,2,1],
+						[2,1,3,4,2,1,3,4,2,1],
+						[2,1,3,4,2,1,3,4,2,1],
+						[2,1,3,4,2,1,3,4,2,1]];
+
+// On récupère les réponses de l'élève sous forme d'un tableau
+$reponses_eleves =  [[2,1,3,4,2,1,3,4,2,1],
+						[2,1,3,4,2,1,3,4,2,1],
+						[2,1,3,4,2,1,3,5,5,5],
+						[2,1,3,4,2,1,3,4,2,1],
+						[2,1,3,4,2,1,3,4,2,1],
+						[2,1,3,4,2,1,3,4,2,1],
+						[2,1,3,4,2,1,3,4,2,1],
+						[2,1,3,4,2,1,3,4,2,1],
+						[2,1,3,4,2,1,3,4,2,1],
+						[4,4,4,4,4,5,6,7,8,9]];
+
+// la difficulté de chaque question est fixée par le professeur
+$difficultes = [0.2, 0.1, 0.3, 0.4, 0.2, 0.1, 0.3, 0.4, 0.2, 0.1];
 
 // On définit les réponses correctes pour chaque question sous forme d'un tableau
 $reponses_correctes = [2,1,3,4,2,1,3,4,2,1];
 
-// On récupère les réponses de l'élève sous forme d'un tableau
-$reponses_eleves =  [[2,1,3,4,2,1,3,4,2,1],
-					[2,1,3,4,2,1,3,4,2,1],
-					[2,1,3,4,2,1,3,4,2,1],
-					[2,1,3,4,2,1,3,4,2,1],
-					[2,1,3,4,2,1,3,4,2,1],
-					[2,1,3,4,2,1,3,4,2,1],
-					[2,1,3,4,2,1,3,4,2,1],
-					[2,1,3,4,2,1,3,4,2,1],
-					[2,1,3,4,2,1,3,4,2,1],
-					[2,1,3,4,2,1,3,4,2,1]];
+for ($y = 0; $y < count($reponses_eleves); $y++) {
+	
+	// On affiche la note finale de l'étudiant
+	$note_finale = bayesian_eval($reponses_correctes, $difficultes, $reponses_eleves, $reponses_prec_eleves);
 
-// On définit l'échelle de difficulté pour chaque question
-$difficultes = prob_difficultes($reponses_correctes, $reponses_eleves);
+	echo "La note finale de l'élève_" . $y . " est de : " . $note_finale[$y];
 
-// On affiche la note finale de l'étudiant
-$note_finale = bayesian_eval($reponses_correctes, $difficultes, $reponses_eleves[0]);
-echo "La note finale de l'élève_0 est de : " . $note_finale;
+	echo "<br/>";echo "<br/>";
 
-echo "<br/>";echo "<br/>";
-
-$note_finale = bayesian_eval($reponses_correctes, $difficultes, $reponses_eleves[1]);
-echo "La note finale de l'élève_1 est de : " . $note_finale;
+}
